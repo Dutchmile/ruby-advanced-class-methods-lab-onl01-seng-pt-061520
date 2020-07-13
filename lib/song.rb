12b20654df3ac3ecbe0a83055911192d0d31b703
Christopher Dutcher <cadutcher@icloud.com>
2020-07-13 21:58:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,9 +36,11 @@ class Song
   end
 
   def Song.find_or_create_by_name(name)
+    if self.find_by_name(name) = nil
+    self.create_by_name(name)
+  else
     self.find_by_name(name)
-    self.create_by_name(name) if self.find_by_name(name) = nil
-
+  end
   end
 
 
