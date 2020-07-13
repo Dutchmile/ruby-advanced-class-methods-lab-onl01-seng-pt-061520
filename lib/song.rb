2d68f1ec300750ea5bc8fd98839e6fd3f91e69a0
Christopher Dutcher <cadutcher@icloud.com>
2020-07-13 21:25:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,6 @@ class Song
   def Song.new_by_name(name)
     song = self.new
     song.name = name
-    song.save
     song
   end
 
@@ -30,5 +29,8 @@ class Song
     song
   end
 
+  def Song.find_by_name(name)
+    @@all.find {|song| song == name}
+  end
 
 end
